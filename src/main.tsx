0f33792d87b4144a25fd495fd0fa613fcf1f19f8
canertekdal <caner.tekdal@gmail.com>
2026-08-20 15:41:21 +0300

    Add bulletproof DOM patch for Google Chrome Translate removeChild exception

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,42 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Google Chrome Translate / DeepL / Browser Extension DOM reconciliation protection
+if (typeof window !== 'undefined' && typeof Node === 'function' && Node.prototype) {
+  const originalRemoveChild = Node.prototype.removeChild;
+  Node.prototype.removeChild = function <T extends Node>(child: T): T {
+    if (!child || child.parentNode !== this) {
+      if (child && child.parentNode) {
+        try {
+          return child.parentNode.removeChild(child) as T;
+        } catch (e) {
+          return child;
+        }
+      }
+      return child;
+    }
+    return originalRemoveChild.call(this, child) as T;
+  };
+
+  const originalInsertBefore = Node.prototype.insertBefore;
+  Node.prototype.insertBefore = function <T extends Node>(newNode: T, referenceNode: Node | null): T {
+    if (referenceNode && referenceNode.parentNode !== this) {
+      if (referenceNode.parentNode) {
+        try {
+          return referenceNode.parentNode.insertBefore(newNode, referenceNode) as T;
+        } catch (e) {
+          return this.appendChild(newNode) as T;
+        }
+      }
+      return this.appendChild(newNode) as T;
+    }
+    return originalInsertBefore.call(this, newNode, referenceNode) as T;
+  };
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
